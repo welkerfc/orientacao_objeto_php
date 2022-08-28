d6baf9d441da4255157c773b951dbdb84bcd4ebd
welkerfc <welkerfc@icloud.com>
2022-08-28 02:02:20 -0300

    Inserindo private

--- index.php
+++ index.php
@@ -2,13 +2,45 @@
 
 class Produto{
 
-  public $descricao;
-  public $preco;
+  private $descricao;
+  private $preco;
 
-  public function(){
-    
+  public function getDetalhes(){
+
+    return "O produto {$this->descricao} custa {$this->preco}";
+
+  }
+
+  public function setDescricao($valor){
+    $this->descricao = $valor;
+  }
+
+
+  public function setPreco($valor){
+    $this->preco = $valor;
+
+  }
+
+  public function getDescicao($valor){
+    return $this->descricao;
+  }
+
+  public function getPreco($valor){
+    return $this->preco;
+
   }
 
 }
+
+$p1 = new Produto;
+$p1->setDescricao('Livro');
+$p1->setPreco(5);
+
+//var_dump($p1);
+
+
+echo $p1->getDetalhes();
+
+
 
 ?>
